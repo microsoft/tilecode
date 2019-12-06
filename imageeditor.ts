ac8fb74c8387cad4a00f4d5a091a5594cab3faff
Tom Ball <tballmsft@users.noreply.github.com>
2019-12-06 14:00:09 -0800

    work on image editor UI unification

--- imageeditor.ts
+++ imageeditor.ts
@@ -1,6 +1,8 @@
 namespace tileWorldEditor {
     const colorSize = 8;
     const paintSize = 6;
+    const colorsY = 30;
+    const colorsX = 5;
     export class ImageEditor {
         private color: boolean;
         private colorCursor: Sprite;
@@ -8,12 +10,12 @@ namespace tileWorldEditor {
         private selectedColor: number;
         private original: Image; // 16x16
         private image: Image;    // 16x16
-        private tileMap: Image;  // whole screen
+        private screen: Image;  // whole screen
         constructor(private manager: SpriteManager, s: Sprite) {
             this.color = true;
             this.colorCursor = sprites.create(colorCursor)
-            this.colorCursor.x = colorSize >> 1
-            this.colorCursor.y = colorSize << 1
+            this.colorCursor.x = colorsX + colorSize >> 1
+            this.colorCursor.y = colorsY + colorSize << 1
             this.selectedColor = 0;
             this.paintCursor = sprites.create(paintCursor)
             this.paintCursor.x = paintSize * 5 + 2 
@@ -21,11 +23,11 @@ namespace tileWorldEditor {
             this.paintCursor.setFlag(SpriteFlag.Invisible, true)
             this.original = s.image;
             this.image = s.image; // i.clone();
-            this.tileMap = image.create(160, 120)
-            scene.setBackgroundImage(this.tileMap)
+            this.screen = image.create(160, 120)
+            scene.setBackgroundImage(this.screen)
             controller.left.onEvent(ControllerButtonEvent.Pressed, () => {
                 if (this.color) {
-                    if (this.colorCursor.x > colorSize)
+                    if (this.colorCursor.x > colorsX + colorSize)
                         this.colorCursor.x -= colorSize
                 } else {
                     if (this.paintCursor.x > paintSize * 6 - 1)
@@ -38,7 +40,7 @@ namespace tileWorldEditor {
             })
             controller.right.onEvent(ControllerButtonEvent.Pressed, () => {
                 if (this.color) {
-                    if (this.colorCursor.x < colorSize)
+                    if (this.colorCursor.x < colorsX + colorSize)
                         this.colorCursor.x += colorSize
                     else {
                         // transition cursor to paint editor
@@ -51,7 +53,7 @@ namespace tileWorldEditor {
             })
             controller.up.onEvent(ControllerButtonEvent.Pressed, () => {
                 if (this.color) {
-                    if (this.colorCursor.y > (colorSize << 1) + (colorSize-1))
+                    if (this.colorCursor.y > colorsY + (colorSize << 1) + (colorSize-1))
                         this.colorCursor.y -= colorSize
                 } else {
                     if (this.paintCursor.y > (paintSize * 3 + 1))
@@ -60,7 +62,7 @@ namespace tileWorldEditor {
             })
             controller.down.onEvent(ControllerButtonEvent.Pressed, () => {
                 if (this.color) {
-                    if (this.colorCursor.y < (colorSize << 1) + colorSize * (colorSize-1))
+                    if (this.colorCursor.y < colorsY + (colorSize << 1) + colorSize * (colorSize-1))
                         this.colorCursor.y += colorSize
                 } else {
                     if (this.paintCursor.y < (paintSize*2) + 2 + paintSize * 15)
@@ -91,39 +93,41 @@ namespace tileWorldEditor {
             this.color = color;
         }
         private update() {
-            this.tileMap.fill(0)
-            this.tileMap.drawImage(paint, 0, 100)
+            this.screen.fill(12);
+            this.screen.fillRect(0, 0, 16, 16, 11);
+            this.screen.drawTransparentImage(paint, 0, 0)
+            //this.screen.fill(0)
             // draw the 16 colors
             for (let row = 0; row < 8; row++) {
                 for (let col = 0; col < 2; col++) {
                     let color = row * 2 + col
-                    let yOffset = colorSize + (colorSize >> 1)
-                    this.tileMap.fillRect(col * colorSize + 1, yOffset + row * colorSize + 1, colorSize-2, colorSize-2, color)
+                    let yOffset = colorsY + colorSize + (colorSize >> 1)
+                    this.screen.fillRect(colorsX + col * colorSize + 1, yOffset + row * colorSize + 1, colorSize-2, colorSize-2, color)
                     if (this.selectedColor == color) {
-                        this.tileMap.drawRect(col * colorSize, yOffset + row * colorSize, colorSize, colorSize, 1)
+                        this.screen.drawRect(colorsX + col * colorSize, yOffset + row * colorSize, colorSize, colorSize, 1)
                     }
                 }
             }
             // take care of transparent
-            this.tileMap.fillRect(1, 13, 3, 3, 13)
-            this.tileMap.fillRect(4, 16, 3, 3, 13)
+            this.screen.fillRect(1, 13, 3, 3, 13)
+            this.screen.fillRect(4, 16, 3, 3, 13)
             // frame the sprite editor
-            this.tileMap.drawRect(28, 10, paintSize * 16 + (paintSize - 2), paintSize * 16 + (paintSize -2), 1)
+            this.screen.drawRect(28, 10, paintSize * 16 + (paintSize - 2), paintSize * 16 + (paintSize -2), 1)
             // draw the sprite editor
             for (let row = 0; row < this.image.height; row++) {
                 let y = (paintSize << 1) + row * paintSize
                 for (let col = 0; col < this.image.width; col++) {
                     let x = paintSize * 5 + col * paintSize
                     let color = this.image.getPixel(col, row)
-                    this.tileMap.fillRect(x, y, paintSize-1, paintSize-1, color)
+                    this.screen.fillRect(x, y, paintSize-1, paintSize-1, color)
                     if (color == 0) {
-                        this.tileMap.fillRect(x, y, (paintSize >> 1) -1, (paintSize >> 1) -1, 13)
-                        this.tileMap.fillRect(x + (paintSize >> 1), y + (paintSize >> 1), (paintSize >> 1)-1, (paintSize >> 1)-1, 13)
+                        this.screen.fillRect(x, y, (paintSize >> 1) -1, (paintSize >> 1) -1, 13)
+                        this.screen.fillRect(x + (paintSize >> 1), y + (paintSize >> 1), (paintSize >> 1)-1, (paintSize >> 1)-1, 13)
                     }
                 }
             }
             // draw the sprite
-            this.tileMap.drawImage(this.image, 134, 12)
+            this.screen.drawImage(this.image, 134, 12)
         }
         private closeMenu(command: string) {
             if (command) {
