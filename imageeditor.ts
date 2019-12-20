99310cfe5e422c86a96117588908c549c5263ab9
Tom Ball <tballmsft@users.noreply.github.com>
2019-12-20 10:15:17 -0800

    simplify image editor

--- imageeditor.ts
+++ imageeditor.ts
@@ -4,10 +4,9 @@ namespace tileworld {
     const paintSize = 6;
     const colorsY = 30;
     const colorsX = 5;
-    enum CursorType { Regular, Color, Paint};
+    enum CursorType { Color, Paint};
     export class ImageEditor extends BackgroundBase {
         private cursorType: CursorType;         // are we selecting a color or painting?
-        private cursor: Sprite;
         private colorCursor: Sprite;
         private paintCursor: Sprite;
         private selectedColor: number;
@@ -15,10 +14,6 @@ namespace tileworld {
         constructor(private p: Project, private kind: number) {
             super();
             this.cursorType= CursorType.Color;
-            this.cursor = sprites.create(cursorIn);
-            this.cursor.x = colorsX + 8;
-            this.cursor.y = yoff + 16 + 8;
-            this.cursor.setFlag(SpriteFlag.Invisible, true);
 
             this.colorCursor = sprites.create(colorCursor)
             this.colorCursor.x = colorsX  + (colorSize>>1);
@@ -35,7 +30,7 @@ namespace tileworld {
                 if (this.cursorType== CursorType.Color) {
                     if (this.colorCursor.x > colorsX + colorSize)
                         this.colorCursor.x -= colorSize
-                } else if (this.cursorType== CursorType.Paint) {
+                } else  {
                     if (this.paintCursor.x > paintSize * 6 - 1)
                         this.paintCursor.x -= paintSize
                     else {
@@ -52,40 +47,27 @@ namespace tileworld {
                         // transition cursor to paint editor
                         this.setCursor(CursorType.Paint);
                     }
-                } else if (this.cursorType== CursorType.Paint) {
+                } else {
                     if (this.paintCursor.x < (paintSize*5 +2) + paintSize * 15)
                         this.paintCursor.x += paintSize
-                } else {
-                    this.setCursor(CursorType.Paint);
                 }
             })
             controller.up.onEvent(ControllerButtonEvent.Pressed, () => {
                 if (this.cursorType== CursorType.Color) {
                     if (this.colorCursor.y > colorsY + (colorSize << 1) + (colorSize-1))
-                        this.colorCursor.y -= colorSize
-                    else {
-                        this.setCursor(CursorType.Regular);
-                    }
-                } else if (this.cursorType== CursorType.Paint) { 
+                        this.colorCursor.y -= colorSize;
+                } else { 
                     if (this.paintCursor.y > (paintSize * 3 + 1))
                         this.paintCursor.y -= paintSize
-                } else {
-                    if (this.cursor.y > yoff + 16)
-                        this.cursor.y -= 16;
                 }
             });
             controller.down.onEvent(ControllerButtonEvent.Pressed, () => {
                 if (this.cursorType== CursorType.Color) {
                     if (this.colorCursor.y < colorsY + (colorSize << 1) + colorSize * (colorSize-1))
                         this.colorCursor.y += colorSize
-                } else if (this.cursorType== CursorType.Paint) {
+                } else {
                     if (this.paintCursor.y < (paintSize*2) + 2 + paintSize * 15)
                         this.paintCursor.y += paintSize
-                } else {
-                    if (this.cursor.y < yoff + 17)
-                        this.cursor.y += 16;
-                    else 
-                        this.setCursor(CursorType.Color);
                 }
             });
             controller.A.onEvent(ControllerButtonEvent.Pressed, () => {
@@ -94,17 +76,11 @@ namespace tileworld {
                     let row = ((this.colorCursor.y - (colorSize << 1) - colorsY) / colorSize) | 0x0
                     this.selectedColor = row * 2 + col
                     this.update()
-                } else if (this.cursorType== CursorType.Paint) {
+                } else {
                     let col = ((this.paintCursor.x - (paintSize*5 + 2)) / paintSize) | 0x0
                     let row = ((this.paintCursor.y - (paintSize*2 + 2)) / paintSize) | 0x0
                     this.image.setPixel(col, row, this.selectedColor)
                     this.update()
-                } else {
-                    let row = (this.cursor.y - yoff) >> 4;
-                    if (row == 0) {
-                        this.saveAndPop();
-                    } else
-                        this.setCursor(CursorType.Paint)
                 }
             });
             controller.B.onEvent(ControllerButtonEvent.Pressed, () => {
@@ -122,7 +98,6 @@ namespace tileworld {
         }
         
         private setCursor(ct: CursorType) {
-            this.cursor.setFlag(SpriteFlag.Invisible, ct != CursorType.Regular);
             this.colorCursor.setFlag(SpriteFlag.Invisible, ct != CursorType.Color);
             this.paintCursor.setFlag(SpriteFlag.Invisible, ct != CursorType.Paint);
             this.cursorType= ct;
@@ -130,9 +105,8 @@ namespace tileworld {
 
         public update() {
             screen.fill(0);
-            screen.fillRect(colorsX, yoff+16, 16, 16, 11);
-            screen.drawTransparentImage(map, colorsX, yoff);
-            screen.drawTransparentImage(paint, colorsX, yoff+16)
+            screen.fillRect(colorsX, yoff, 16, 16, 11);
+            screen.drawTransparentImage(paint, colorsX, yoff)
             //screen.fill(0)
             // draw the 16 colors
             for (let row = 0; row < 8; row++) {
