9f7a371d493f10801e5afe9d81d75c9c14b32e25
Tom Ball <tballmsft@users.noreply.github.com>
2020-01-28 16:04:03 -0800

    new release

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
 // to clear the tileworld programs from settings (in browser, or on device), uncomment the following line
 // settings.clear();
+
 let TileWorld = new tileworld.LoadScreen(sample.project);
