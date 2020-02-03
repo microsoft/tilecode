25cdc0e83a7e07c595139fe2cfcea034faae5ad3
Tom Ball <tballmsft@users.noreply.github.com>
2020-02-03 12:52:54 -0800

    more stuff in galleries

--- gallery.ts
+++ gallery.ts
@@ -69,6 +69,79 @@ namespace tileworld {
         . . . f d d c d d b b d f . . .
         . . . . f f f f f f f f f . . .
     `;
+    const player = img`
+        . . . . . . f f f f . . . . . .
+        . . . . f f f 2 2 f f f . . . .
+        . . . f f f 2 3 2 2 f f f . . .
+        . . f f f e e e e e e f f f . .
+        . . f f e 2 2 2 2 2 2 e e f . .
+        . . f e 2 f f f f f f 2 e f . .
+        . . f f f f e e e e f f f f . .
+        . f f e f b f 4 4 f b f e f f .
+        . f e e 4 1 f d d f 1 4 e e f .
+        . . f e e d d d d d d e e f . .
+        . . . f e e 4 4 4 4 e e f . . .
+        . . e 4 f 2 2 2 2 2 2 f 4 e . .
+        . . 4 d f 2 2 2 2 2 2 f d 4 . .
+        . . 4 4 f 4 4 5 5 4 4 f 4 4 . .
+        . . . . . f f f f f f . . . . .
+        . . . . . f f . . f f . . . . .
+    `;
+    const diamond = img`
+        . . . . . . . . . . . . . . . .
+        . . . . 8 8 8 8 8 8 8 8 . . . .
+        . . . 8 8 8 8 9 9 9 1 1 . . . .
+        . . 8 8 8 8 9 9 9 9 1 1 1 1 . .
+        . 8 8 8 8 8 8 9 9 1 1 1 1 1 1 .
+        . 8 8 8 8 8 8 8 1 1 1 1 1 1 1 .
+        . 9 9 9 9 9 9 9 9 9 9 9 9 9 9 .
+        . 9 9 9 9 9 9 9 1 1 1 1 1 1 1 .
+        . 9 9 9 9 9 9 9 1 1 1 1 1 1 1 .
+        . . 9 9 9 9 9 9 1 1 1 1 1 1 . .
+        . . . 9 9 9 9 9 1 1 1 1 1 . . .
+        . . . . 9 9 9 9 1 1 1 1 . . . .
+        . . . . . 9 9 9 1 1 1 . . . . .
+        . . . . . . 9 9 1 1 . . . . . .
+        . . . . . . . 9 1 . . . . . . .
+        . . . . . . . . . . . . . . . .
+    `;
+    const boulder = img`
+        . . . . . c c b b b . . . . . .
+        . . . . c b d d d d b . . . . .
+        . . . . c d d d d d d b b . . .
+        . . . . c d d d d d d d d b . .
+        . . . c b b d d d d d d d b . .
+        . . . c b b d d d d d d d b . .
+        . c c c c b b b b d d d b b b .
+        . c d d b c b b b b b b b b d b
+        c b b d d d b b b b b d d b d b
+        c c b b d d d d d d d b b b d c
+        c b c c c b b b b b b b d d c c
+        c c b b c c c c b d d d b c c b
+        . c c c c c c c c c c c b b b b
+        . . c c c c c b b b b b b b c .
+        . . . . . . c c b b b b c c . .
+        . . . . . . . . c c c c . . . .
+    `;
+    const enemy = img`
+        . . . . . . . f f f f . . . . .
+        . . . . . f f 1 1 1 1 f f . . .
+        . . . . f b 1 1 1 1 1 1 b f . .
+        . . . . f 1 1 1 1 1 1 1 1 f . .
+        . . . f d 1 1 1 1 1 1 1 1 d f .
+        . 7 . f d 1 1 1 1 1 1 1 1 d f .
+        7 . . f d 1 1 1 1 1 1 1 1 d f .
+        7 . . f d 1 1 1 1 1 1 1 1 d f .
+        7 . . f d d d 1 1 1 1 d d d f f
+        7 7 . f b d b f d d f b d b f c
+        7 7 7 f c d c f 1 1 f c d c f b
+        . 7 7 f f f b d b 1 b d f f c f
+        . f c b 1 b c f f f f f f . . .
+        . f 1 c 1 c 1 f f f f f f . . .
+        . f d f d f d f f f f f . . . .
+        . . f . f . f . . . . . . . . .
+    `;
+
     const brick = img`
         b d d d d d d c b d d d d d d c
         d b b b b b b c d b b b b b b c
@@ -123,7 +196,7 @@ namespace tileworld {
         6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 7
         6 6 7 6 6 6 6 6 6 6 6 6 6 6 6 7
     `;
-    const dirt = img`
+    const dirt2 = img`
         d d d d d d d d d d d d d d d d
         d d d 1 1 d d d d d d d d b d d
         d d d 1 1 d d d d d d d d d d d
@@ -141,10 +214,83 @@ namespace tileworld {
         d d d d d d d d d d d d d d d d
         d d d d d d d d d d d d d d b d
     `;
+    const wall = img`
+        d d d d d d d d d d d d d d d 8
+        d 6 6 6 8 8 8 6 6 6 6 6 6 6 8 8
+        d 6 6 8 6 6 6 8 6 6 6 6 6 6 8 8
+        d 6 8 6 8 8 8 6 8 8 8 8 8 8 8 8
+        d 8 6 8 8 d 8 8 6 6 6 6 6 6 8 8
+        d 8 6 8 d d d 8 6 8 8 8 8 8 6 8
+        d 8 6 8 8 d 8 8 6 6 6 6 6 6 8 8
+        d 6 8 6 8 8 8 6 8 8 8 8 8 8 8 8
+        d 6 6 6 6 6 6 6 6 8 6 6 6 6 8 8
+        d 8 8 8 6 6 6 6 6 8 8 6 6 8 6 8
+        d 6 6 6 6 6 6 6 6 8 8 8 8 8 6 8
+        d 8 8 8 6 6 6 6 6 6 6 6 6 6 6 8
+        d 6 6 6 6 6 6 6 6 6 6 6 6 6 6 8
+        d 8 8 8 8 6 6 6 6 8 8 8 8 8 6 8
+        d 6 6 6 6 6 6 6 8 8 6 6 6 8 6 8
+        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8
+    `;
+    const wall2 = img`
+        c c c c c c c c c c c c c c c 8
+        c b b b 8 8 8 b b b b b b b 8 8
+        c b b 8 b b b 8 b b b b b b 8 8
+        c b 8 b 8 8 8 b 8 8 8 8 8 8 8 8
+        c 8 b 8 8 6 8 8 b b b b b b 8 8
+        c 8 b 8 6 6 6 8 b 8 8 8 8 8 b 8
+        c 8 b 8 8 6 8 8 b b b b b b 8 8
+        c b 8 b 8 8 8 b 8 8 8 8 8 8 8 8
+        c b b b b b b b b 8 b b b b 8 8
+        c 8 8 8 b b b b b 8 8 b b 8 b 8
+        c b b b b b b b b 8 8 8 8 8 b 8
+        c 8 8 8 b b b b b b b b b b b 8
+        c b b b b b b b b b b b b b b 8
+        c 8 8 8 8 b b b b 8 8 8 8 8 b 8
+        c b b b b b b b 8 8 b b b 8 b 8
+        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8
+    `;
+    const dirt = img`
+        f e e e e e f e e e e 4 4 4 4 e
+        e e 4 4 e e e f f f e e e e e e
+        e 4 4 4 4 4 e e f f f f f e e e
+        e 4 4 4 4 4 4 e f e e e e e f e
+        e 4 4 4 4 4 4 e f e 4 4 4 4 e f
+        e e 4 4 4 4 4 f e 4 4 4 4 4 4 e
+        e e e 4 4 4 e e e 4 4 4 4 4 4 e
+        f f e e e e e f e 4 4 4 4 4 4 e
+        f e e e 4 4 4 e f e 4 4 4 4 e e
+        f e e 4 4 4 4 4 e e e e 4 4 e f
+        e e 4 4 4 4 4 4 4 e f e e e e f
+        f e 4 4 4 4 4 4 4 e e f f f e e
+        f e 4 4 4 4 4 4 4 e f e e e e f
+        e f e 4 4 4 4 4 e f e 4 4 e e e
+        e e f e 4 4 4 e f e 4 4 4 4 e e
+        f e e f e e e f e 4 4 4 4 4 4 e
+    `;
+     const space = img`
+         f f f f f f f f f f f c c c c f
+         f f c c f f f f f f f f f f f f
+         f c c c c c f f f f f f f f f f
+         f c c c c c c f f f f f f f f f
+         f c c c c c c f f f c c c c f f
+         f f c c c c c f f c c c c c c f
+         f f f c c c f f f c c c c c c f
+         f f f f f f f f f c c c c c c f
+         f f f f c c c f f f c c c c f f
+         f f f c c c c c f f f f c c f f
+         f f c c c c c c c f f f f f f f
+         f f c c c c c c c f f f f f f f
+         f f c c c c c c c f f f f f f f
+         f f f c c c c c f f f c c f f f
+         f f f f c c c f f f c c c c f f
+         f f f f f f f f f c c c c c c f
+     `;
+
 
     // up to 15 max
-    export const gallerySprites = [cat, fish, dog, chimp, trophyUp, debug, eat ];
-    export const galleryTiles = [brick, grass, water, dirt ];
+    export const gallerySprites = [cat, fish, dog, chimp, player, diamond, boulder, enemy, trophyUp, debug, eat ];
+    export const galleryTiles = [brick, grass, water, dirt, dirt2, space, wall];
 
     export class Gallery extends RuleVisualsBase {
         private current: Image;
