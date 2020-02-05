d38622068061926632ba8899e3c97945d31b6f9d
Tom Ball <tballmsft@users.noreply.github.com>
2020-02-04 16:41:57 -0800

    get rid of settings comment

--- main.ts
+++ main.ts
@@ -1,4 +1,1 @@
-// to clear the tileworld programs from settings (in browser, or on device), uncomment the following line
-// settings.clear();
-
 let TileWorld = new tileworld.LoadScreen(sample.project);
