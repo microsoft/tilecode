2de00f520bad8d68df60789a9c3cc6026cf29820
Tom Ball <tballmsft@users.noreply.github.com>
2020-04-21 11:23:00 -0700

    debugging

--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
 // see games.ts for built-in games
 
-let TileWorld = new tileworld.LoadScreen();
+// let TileWorld = new tileworld.LoadScreen();
+
+// basic test 
+
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    settings.clear();
+    game.splash("cleared")
+})
+
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    let buf = settings.readBuffer("TW1-WBackM");
+    if (buf) {
+        game.splash("buf found")
+    } else {
+        game.splash("buf not found")
+    }
+})
+
