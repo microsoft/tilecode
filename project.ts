b1bd8f2b25685f619a84722b09dbd8d1ca5e0fef
Tom Ball <tballmsft@users.noreply.github.com>
2020-04-23 15:28:06 -0700

    put sprites altogether

--- project.ts
+++ project.ts
@@ -324,33 +324,13 @@ namespace tileworld {
         return buf;
     }
 
-    const player = img`
-        . . . . . . f f f f . . . . . .
-        . . . . f f f 2 2 f f f . . . .
-        . . . f f f 2 3 2 2 f f f . . .
-        . . f f f e e e e e e f f f . .
-        . . f f e 2 2 2 2 2 2 e e f . .
-        . . f e 2 f f f f f f 2 e f . .
-        . . f f f f e e e e f f f f . .
-        . f f e f b f 4 4 f b f e f f .
-        . f e e 4 1 f d d f 1 4 e e f .
-        . . f e e d d d d d d e e f . .
-        . . . f e e 4 4 4 4 e e f . . .
-        . . e 4 f 2 2 2 2 2 2 f 4 e . .
-        . . 4 d f 2 2 2 2 2 2 f d 4 . .
-        . . 4 4 f 4 4 5 5 4 4 f 4 4 . .
-        . . . . . f f f f f f . . . . .
-        . . . . . f f . . f f . . . . .
-    `;
-
     export function emptyProject(prefix: string) {
         let fixed: Image[] = []; 
         let movable: Image[] = [];
         for(let f=0;f<4;f++) {
             fixed.push(galleryTiles[f].clone());
         }
-        movable.push(player.clone());
-        for (let f = 0; f < 3; f++) {
+        for (let f = 0; f < 4; f++) {
             movable.push(gallerySprites[f].clone());
         }
         let rules: Rule[] = [];
