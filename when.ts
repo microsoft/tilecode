eb9951d8c47da8a2520bf874d8eeea862da63020
Tom Ball <tballmsft@users.noreply.github.com>
2020-05-10 19:06:12 -0700

    thoughts on tile widget

---
+++ when.ts
@@ -0,0 +1,16 @@
+namespace tileworld {
+
+    export class TileWidget {
+        // has an absolute coordinate in parent space
+        // occupies some contiguous area (usually, a rectangle)
+        // has a parent that will pass down cursor information and button press information
+        // has its own saved cursor position
+        // can be active/inactive, visible/invisible
+        // model/view separation???
+    }
+
+    // widget for when section (3x3 pattern)
+    export class WhenPattern extends TileWidget {
+
+    }
+} 
