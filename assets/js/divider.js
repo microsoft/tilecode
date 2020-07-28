24e869f7abe6a5b37852e6961a08d46e2351e2b8
Tom Ball <tballmsft@users.noreply.github.com>
2020-07-27 23:40:46 -0700

    some semis

--- assets/js/divider.js
+++ assets/js/divider.js
@@ -2,11 +2,11 @@ function setWidths() {
     var t = document.body.clientWidth
       , n = Math.floor(t * ratio)
       , i = Math.max(t - n - dividerWidth, 4);
-    manualContent.style.width = n + "px",
-    divider.style.left = n + "px",
-    divider.style.width = dividerWidth + "px",
-    simulatorContent.style.left = n + dividerWidth + "px",
-    simulatorContent.style.width = i + "px"
+    manualContent.style.width = n + "px";
+    divider.style.left = n + "px";
+    divider.style.width = dividerWidth + "px";
+    simulatorContent.style.left = n + dividerWidth + "px";
+    simulatorContent.style.width = i + "px";
 }
 
 function startDrag() {
@@ -15,19 +15,21 @@ function startDrag() {
     divider.onmouseover = null;
     divider.onmouseout = null;
     function newMouseMove(e) {
-        e || (e = window.event),
-        e.preventDefault(),
-        ratio = ((e.clientX - dividerWidth) / 2) / document.body.clientWidth,
-        ratio < .1 && (ratio = .1),
-        ratio > .9 && (ratio = .9),
+        if (!e)
+            e = window.event;
+        e.preventDefault();
+        ratio = (e.clientX - dividerWidth / 2) / document.body.clientWidth;
+        if (ratio < .1) ratio = .1;
+        if (ratio > .9) ratio = .9;
         setWidths();
     }
     function newMouseUp(e) {
-        e || (e = window.event),
-        e.preventDefault(),
-        document.body.removeEventListener('mousemove', newMouseMove, true),
-        document.body.removeEventListener('mouseup', newMouseUp, true),
-        divider.onmouseover = n,
+        if (!e)
+            e = window.event;
+        e.preventDefault();
+        document.body.removeEventListener('mousemove', newMouseMove, true);
+        document.body.removeEventListener('mouseup', newMouseUp, true);
+        divider.onmouseover = n;
         divider.onmouseout = t;
     }
     document.body.addEventListener('mousemove', newMouseMove, true);
@@ -39,15 +41,16 @@ var divider = document.getElementById("divider")
   , simulatorContent = document.getElementById("simframe")
   , ratio = .6
   , dividerWidth = 4;
-window.onresize = setWidths
+
+window.onresize = setWidths;
 setWidths()
 
 divider.onmouseover = function() {
-    document.body.style.cursor = "w-resize",
-    divider.onmousedown = startDrag
+    document.body.style.cursor = "w-resize";
+    divider.onmousedown = startDrag;
 }
 
 divider.onmouseout = function() {
-    document.body.style.cursor = "default",
-    divider.onmousedown = null
+    document.body.style.cursor = "default";
+    divider.onmousedown = null;
 }
