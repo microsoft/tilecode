737811a3d179d84c0456509aeb87a398c77cef08
Tom Ball <tballmsft@users.noreply.github.com>
2020-07-28 21:08:09 -0700

    reset code

--- assets/js/divider.js
+++ assets/js/divider.js
@@ -2,53 +2,50 @@ function setWidths() {
     var t = document.body.clientWidth
       , n = Math.floor(t * ratio)
       , i = Math.max(t - n - dividerWidth, 8);
-    leftContent.style.width = n + "px";
-    divider.style.left = n + "px";
-    divider.style.width = dividerWidth + "px";
-    rightContent.style.left = n + dividerWidth + "px";
-    rightContent.style.width = i + "px";
+    pageContent.style.width = n + "px",
+    divider.style.left = n + "px",
+    divider.style.width = dividerWidth + "px",
+    pexForFunContent.style.left = n + dividerWidth + "px",
+    pexForFunContent.style.width = i + "px"
 }
-
-let drag = false;
 function startDrag() {
-    drag = true;
-    leftContent.style.display = "none";
-    rightContent.style.display = "none";
-    document.body.onmousemove = function newMouseMove(e) {
-        console.log("mousemove");
-        e || (e = window.event);
-        ratio = (e.clientX - dividerWidth / 2) / document.body.clientWidth;
-        ratio < .1 && (ratio = .1);
-        ratio > .9 && (ratio = .9);
-        setWidths();
+    pageContent.style.visibility = "hidden",
+    pexForFunContent.style.visibility = "hidden";
+    var n = divider.onmouseover
+      , t = divider.onmouseout;
+    divider.onmouseover = null,
+    divider.onmouseout = null,
+    document.body.onmousemove = function(n) {
+        n || (n = window.event),
+        ratio = (n.clientX - dividerWidth / 2) / document.body.clientWidth,
+        ratio < .1 && (ratio = .1),
+        ratio > .9 && (ratio = .9),
+        setWidths()
     }
-    document.body.onmouseup = function newMouseUp(e) {
-        console.log("mouseup")
-        leftContent.style.display = "block";
-        rightContent.style.display = "block";
-        document.body.onmousemove = null;
-        document.body.onmouseup = null;
-        drag = false;
+    ,
+    document.body.onmouseup = function() {
+        document.body.onmousemove = null,
+        document.body.onmouseup = null,
+        pageContent.style.visibility = "inherit",
+        pexForFunContent.style.visibility = "inherit",
+        divider.onmouseover = n,
+        divider.onmouseout = t
     }
 }
-
 var divider = document.getElementById("divider")
-  , leftContent = document.getElementById("manual")
-  , rightContent = document.getElementById("simframe")
+  , pageContent = document.getElementById("manual")
+  , pexForFunContent = document.getElementById("simframe")
   , ratio = .6
   , dividerWidth = 8;
-
-window.onresize = setWidths;
-setWidths()
-
+window.onresize = setWidths,
+setWidths(),
 divider.onmouseover = function() {
-    if (drag) return;
-    document.body.style.cursor = "w-resize";
-    divider.onmousedown = startDrag;
+    document.body.style.cursor = "w-resize",
+    divider.onmousedown = startDrag
 }
-
+,
 divider.onmouseout = function() {
-    if (drag) return;
-    document.body.style.cursor = "default";
-    divider.onmousedown = null;
+    document.body.style.cursor = "default",
+    divider.onmousedown = null
 }
+;
